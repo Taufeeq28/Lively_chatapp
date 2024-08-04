32243285fcee9b516f091381ade033796ad6a60b
Taufeeq28 <sameerahmed6686@gmail.com>
2024-08-04 06:43:38 -0500

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,23 +1,23 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/Lively_chatapp/', // Make sure this matches your repository name
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ['react'],
-        },
-      },
-    },
-  },
- });
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
-// })
+//   base: '/Lively_chatapp/', // Make sure this matches your repository name
+//   build: {
+//     rollupOptions: {
+//       output: {
+//         manualChunks: {
+//           react: ['react'],
+//         },
+//       },
+//     },
+//   },
+//  });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
