29202b42a30a644a073386604563c5d0b0922601
Taufeeq28 <sameerahmed6686@gmail.com>
2024-08-04 06:55:42 -0500

    update apiconfig

--- frontend/apiConfig.js
+++ frontend/apiConfig.js
@@ -4,9 +4,10 @@ let baseUrl;
 let socketUrl;
 
 if (import.meta.env.VITE_NODE_ENV === "production") {
-  baseUrl = "lively-chatapp-backend.vercel.app";
-  socketUrl = "lively-chatapp-backend.vercel.app";
-} else {
+  baseUrl = "https://lively-chatapp-backend.vercel.app";
+  socketUrl = "wss://lively-chatapp-backend.vercel.app";
+}
+ else {
   baseUrl = "http://localhost:4000";
   socketUrl = "ws://localhost:4000";
 }
