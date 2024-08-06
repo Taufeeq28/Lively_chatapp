8e90be451300e94d0165fbd98ce70d2754889f54
Taufeeq28 <sameerahmed6686@gmail.com>
2024-08-06 18:21:43 -0500

    update

--- chatapp/middleware/protect.js
+++ chatapp/middleware/protect.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 async function protect(req) {
   return new Promise((resolve, reject) => {
     const token = req.cookies?.authToken;
+    console.log("Received token:", token);
     if (token) {
       jwt.verify(token, process.env.JWTPRIVATEKEY, {}, (err, userData) => {
         if (err) {
