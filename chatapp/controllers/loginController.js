80e51e530c7343101718825d96b100a6105e7354
Taufeeq28 <sameerahmed6686@gmail.com>
2024-08-06 22:45:07 -0500

    update

--- chatapp/controllers/loginController.js
+++ chatapp/controllers/loginController.js
@@ -24,7 +24,7 @@ const loginController = async (req, res) => {
 
     const token = user.generateAuthToken();
     res.status(200).cookie("authToken", token, {
-      httpOnly: true,
+      httpOnly: false,
       sameSite: "none",
       secure: true,
       expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
